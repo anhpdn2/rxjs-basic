caaec5bfb0c53df6ab71783ebbdb0b80d6d5aa73
anhpdn2 <93982926+anhpdn2@users.noreply.github.com>
2022-10-03 20:50:31 +0700

    2 files modified

--- index.ts
+++ index.ts
@@ -2,13 +2,31 @@ import './style.css';
 console.clear();
 
 // begin lesson code
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { fromEvent, interval } from 'rxjs';
+import { map, scan, mapTo, filter } from 'rxjs/operators';
 
 // elem refs
 
-const countdown = document.getElementById('#message');
+const countdown = document.getElementById('countdown');
+const mess = document.getElementById('message');
 
 // streams
 
-const counter$ = interval()
+const counter$ = interval(1000);
+const codeElem = document.getElementById('code');
+const keyup$ = fromEvent(document, 'keyup');
+
+counter$
+  .pipe(
+    mapTo(-1),
+    scan((accumulator, current) => {
+      return accumulator + current;
+    }, 10),
+    filter((x) => x >= 0)
+  )
+  .subscribe((data) => {
+    countdown.innerHTML = data + '';
+    if (data == 0) {
+      mess.innerHTML = 'Liff off';
+    }
+  });
