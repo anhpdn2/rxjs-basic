cb1df0eae009197794b31712c49d4af6b5c1db5a
anhpdn2 <93982926+anhpdn2@users.noreply.github.com>
2022-10-03 21:55:31 +0700

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ console.clear();
 
 // begin lesson code
 import { fromEvent, interval } from 'rxjs';
-import { map, scan, mapTo, filter } from 'rxjs/operators';
+import { map, scan, mapTo, filter, tap } from 'rxjs/operators';
 
 // elem refs
 
@@ -15,6 +15,12 @@ const mess = document.getElementById('message');
 const counter$ = interval(1000);
 const codeElem = document.getElementById('code');
 const keyup$ = fromEvent(document, 'keyup');
+// const keycode$ = keyup$.pipe(
+//   map((event) => event.code),
+//   tap((code) => {
+//     codeElem.innerHTML = code;
+//   })
+// );
 
 counter$
   .pipe(
