a1cc0584b886ce4753067d2c4f19fe2d7c2a0386
anhpdn2 <93982926+anhpdn2@users.noreply.github.com>
2022-10-05 00:33:12 +0700

    2 files modified

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import './style.css';
 console.clear();
 
 // begin lesson code
-import { fromEvent, interval, of } from 'rxjs';
+import { fromEvent, interval, of, empty } from 'rxjs';
 import {
   map,
   scan,
@@ -17,7 +17,10 @@ import {
   mergeMap,
   pluck,
   distinctUntilChanged,
+  delay,
   switchMap,
+  concatMap,
+  catchError,
 } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
@@ -33,12 +36,19 @@ const ti = document.getElementById('text-input');
 const counter$ = interval(1000);
 const codeElem = document.getElementById('code');
 const animalElem = document.getElementById('res-animal');
-const keyup$ = fromEvent(document, 'click').pipe(
-  map((event) => ({ x: event.clientX, y: event.clientY }))
-);
+// const keyup$ = fromEvent(document, 'click').pipe(
+//   map((event) => ({ x: event.clientX, y: event.clientY }))
+// );
 const input$ = fromEvent(ti, 'keyup');
-const clickAbort$ = fromEvent(document.getElementById('takeuntil'), 'click');
-
+// const clickAbort$ = fromEvent(document.getElementById('takeuntil'), 'click');
+// const saveAnswer = (answer) => {
+//   return of(`Save: ${answer}`).pipe(delay(1500));
+// };
+// const radioBtns = document.querySelectorAll('.radio-button');
+// const radio$ = fromEvent(radioBtns, 'click');
+// radio$
+//   .pipe(concatMap((event) => saveAnswer(event.target.value)))
+//   .subscribe(console.log);
 // const keycode$ = counter$.pipe(takeUntil(clickAbort$)).subscribe(console.log);
 
 // counter$
@@ -61,20 +71,22 @@ input$
     pluck('target', 'value'),
     distinctUntilChanged(),
     switchMap((data) => {
-      return ajax.getJSON(`${BASE_URL}?by_name=${data}`);
+      return ajax
+        .getJSON(`${BASE_URL}?by_name=${data}`)
+        .pipe(catchError((err) => empty()));
     })
   )
   .subscribe((data) => {
     animalElem.innerHTML = data.map((d) => d.name).join('<br>');
   });
-keyup$
-  .pipe(
-    mergeMap((data) => {
-      console.log(data);
-      return ajax.post(
-        'https://www.mocky.io/v2/5185415ba171ea3a00704eed',
-        data
-      );
-    })
-  )
-  .subscribe(console.log);
+// keyup$
+//   .pipe(
+//     mergeMap((data) => {
+//       console.log(data);
+//       return ajax.post(
+//         'https://www.mocky.io/v2/5185415ba171ea3a00704eed',
+//         data
+//       );
+//     })
+//   )
+//   .subscribe(console.log);
